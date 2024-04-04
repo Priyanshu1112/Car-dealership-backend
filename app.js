61e610b716786d4748caa8086639b0e1435188a3
Priyanshu1112 <ashu1112sing@gmail.com>
2024-04-04 12:48:21 +0530

    cors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://car-dealership-frontend-bws94vhfu.vercel.app/",
+      "https://car-dealership-frontend-bws94vhfu.vercel.app",
       "*",
     ],
     credentials: true,
