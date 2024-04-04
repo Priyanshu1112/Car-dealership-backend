d6522970aeb6643bf7d828986e1cb715e1b4c767
Priyanshu1112 <ashu1112sing@gmail.com>
2024-04-04 12:04:07 +0530

    Index Route

--- Routes/index.js
+++ Routes/index.js
@@ -5,6 +5,10 @@ const RazorPay = require("../Controller/razorPayController");
 const { isBuyer } = require("../Utils/checkUserType");
 const isAuthenticated = require("../Middleware/authMiddleware");
 
+router.get("/", (req, res) => {
+  res.send("Car-Dealership-Backend");
+});
+
 router.get("/current-user", Index.getCurrentUser);
 
 router.post("/make-payment", RazorPay.makePayment);
